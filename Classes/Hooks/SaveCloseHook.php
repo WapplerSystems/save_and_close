25f8120eddf36eb4b6a58d628f06871617637abb
Sven Wappler <info@wappler.systems>
2023-06-19 23:55:45 +0200

    first commit

---
+++ Classes/Hooks/SaveCloseHook.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace WapplerSystems\SaveAndClose\Hooks;
+
+use TYPO3\CMS\Backend\Template\Components\ButtonBar;
+use TYPO3\CMS\Core\Imaging\Icon;
+use TYPO3\CMS\Core\Imaging\IconFactory;
+use TYPO3\CMS\Core\Localization\LanguageService;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Backend\Template\Components\Buttons\InputButton;
+
+/**
+ *
+ */
+class SaveCloseHook
+{
+    /**
+     * @param array $params
+     * @param $buttonBar
+     * @return array
+     */
+    public function addSaveCloseButton($params, $buttonBar)
+    {
+        $buttons = $params['buttons'];
+        $saveButton = $buttons[ButtonBar::BUTTON_POSITION_LEFT][2][0] ?? null;
+        if ($saveButton instanceof InputButton) {
+            /** @var IconFactory $iconFactory */
+            $iconFactory = GeneralUtility::makeInstance(IconFactory::class);
+
+            $saveCloseButton = $buttonBar->makeInputButton()
+                ->setName('_saveandclosedok')
+                ->setValue('1')
+                ->setForm($saveButton->getForm())
+                ->setTitle($this->getLanguageService()->sL('LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:rm.saveCloseDoc'))
+                ->setIcon($iconFactory->getIcon('actions-document-save-close', Icon::SIZE_SMALL))
+                ->setShowLabelText(true);
+
+            $buttons[ButtonBar::BUTTON_POSITION_LEFT][2][] = $saveCloseButton;
+        }
+        return $buttons;
+    }
+
+    /**
+     * Returns the language service
+     * @return LanguageService
+     */
+    protected function getLanguageService()
+    {
+        return $GLOBALS['LANG'];
+    }
+}
---
+++ ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+
+$EM_CONF['save_and_close'] = [
+    'title' => 'Save and close button',
+    'description' => 'Adds save and close button to all the content elements',
+    'category' => 'be',
+    'author' => 'Sven Wappler',
+    'author_email' => 'typo3@wappler.systems',
+    'state' => 'stable',
+    'uploadfolder' => false,
+    'version' => '12.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.5.0-12.5.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
