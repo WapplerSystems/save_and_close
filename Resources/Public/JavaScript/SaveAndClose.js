af18ec233d483071e279c0c1a20025c04e8b6cc3
Sven Wappler <wappler@wappler.systems>
2025-04-03 17:22:47 +0200

    fix for #8

--- Resources/Public/JavaScript/SaveAndClose.js
+++ Resources/Public/JavaScript/SaveAndClose.js
@@ -1,17 +1,17 @@
-import FormEngine from '@typo3/backend/form-engine.js'
 
 class SaveAndClose {
   constructor () {
     // I have added the data-js attribute to the button so I can select it like that
     this.button = document.querySelector('[data-js=save-and-close-button]')
-    this.#addEvents()
-  }
-
-  #addEvents () {
-    this.button?.addEventListener('click', (e) => {
-      e.preventDefault()
-      FormEngine.saveAndCloseDocument()
-    })
+    if (this.button) {
+      import('@typo3/backend/form-engine.js').then((form) => {
+        this.button.addEventListener('click', (e) => {
+          e.preventDefault()
+          const FormEngine = form.default;
+          FormEngine.saveAndCloseDocument()
+        })
+      })
+    }
   }
 }
 
