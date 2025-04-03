6e5956ee85dbfa8c7102d353374468dbe7eb6ed8
Sven Wappler <wappler@wappler.systems>
2025-04-03 21:11:01 +0200

    clean up

--- Resources/Public/JavaScript/SaveAndClose.js
+++ Resources/Public/JavaScript/SaveAndClose.js
@@ -1,7 +1,6 @@
 
 class SaveAndClose {
   constructor () {
-    // I have added the data-js attribute to the button so I can select it like that
     this.button = document.querySelector('[data-js=save-and-close-button]')
     if (this.button) {
       import('@typo3/backend/form-engine.js').then((form) => {
@@ -14,5 +13,4 @@ class SaveAndClose {
     }
   }
 }
-
 export default new SaveAndClose()
